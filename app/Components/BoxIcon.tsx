9ba0e6cc9aea7e19cfb67d157294bd2b2029ed8c
rajib5570 <rajib5570@gmail.com>
2024-08-12 21:45:36 +0600

    icon added

--- app/Components/BoxIcon.tsx
+++ app/Components/BoxIcon.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { LuCrown } from 'react-icons/lu'
 
 function BoxIcon() {
   return (
-    <div>
-      
+    <div className='size-12 bg-slate-600'>
+      <LuCrown />
     </div>
   )
 }
